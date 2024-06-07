8b543635dcee75c47267307fbd452bae8d443d9c
Aurongojeb Lishad <aurongojeblishad02@gmail.com>
2024-06-07 21:04:07 +0600

    modified

--- backend/userlogin.php
+++ backend/userlogin.php
@@ -4,7 +4,8 @@
 	$query = "insert into users values(null,'$_POST[name]','$_POST[email]','$_POST[password]',$_POST[mobile],'$_POST[address]')";
 	$query_run = mysqli_query($connection,$query);
 ?>
+
 <script type="text/javascript">
-	alert("Greetings you may log in now");
-	//window.location.href = "index.php";
+	alert("Greetings! you may log in now");
+	
 </script>
