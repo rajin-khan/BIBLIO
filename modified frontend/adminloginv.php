13fc3a7cd658381da46ae1028cf1f74dd600f937
Aurongojeb Lishad <aurongojeblishad02@gmail.com>
2024-06-10 23:46:54 +0600

    admin login html modified

--- modified frontend/adminloginv.php
+++ modified frontend/adminloginv.php
@@ -4,29 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Link to your CSS file for styling -->
+    <link rel="normalize" href="css/normalize.css">
+    <link rel="stylesheet" href="adminlogin.css">
+    <link href="https://fonts.cdnfonts.com/css/roseritta" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <h2>Admin Login</h2>
-        <form action="adminlogin.php" method="post">
-            <div class="form-group">
-                <label for="id">ID:</label>
-                <input type="text" id="id" name="id" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <button type="submit" name="login">Login</button>
-        </form>
-        <?php
-        if (isset($wrongPassword) && $wrongPassword) {
-            echo '<br><br><center><span class="alert-danger">Wrong Password !! An email has been sent to you regarding this attempt.</span></center>';
-        } elseif (isset($_POST['login']) && !$wrongPassword) {
-            echo '<br><br><center><span class="alert-danger">Email not found. Please check your email and try again.</span></center>';
-        }
-        ?>
-    </div>
+    <form action="adminlogin.php" method="POST">
+        <img class="background" src="/BIBLIO/frontend/webimages/admin login.png" alt="Background of admin login page">
+
+        <a href="/BIBLIO/frontend/landing.html">
+            <p id="backButton">BACK</p>
+        </a>
+        
+        <input type="text" name="id" id="username" placeholder="Admin ID" required>
+        <input type="password" name="password" id="password" placeholder="Password" required>
+
+        <input type="submit" name="login" id="loginButton" value="LOGIN">
+        
+    </form>
 </body>
 </html>
